05d944518ae57f647cb058c5aad4a2255f4a3899
Jim Moua <jimmoua24@gmail.com>
2021-05-01 20:29:54 -0500

    createCourse helper function test added

---
+++ tests/helpers/Courses/createCourse.test.js
@@ -0,0 +1,22 @@
+const createCourse = require("../../../helpers/Courses/createCourse");
+const createUser = require("../../../helpers/Users/createUser");
+const { Courses } = require("../../../db/Models");
+const dbHelpers = require("../../../db/databaseHelpers");
+
+beforeAll(async() => {
+  await dbHelpers.createTables();
+});
+
+afterAll(async() => {
+  await dbHelpers.wipeDBTables();
+});
+
+describe("The createCourse fn", () => {
+  it("should insert a course", async() => {
+    await createUser("1", "tutor@email.com", "1234", "tutor");
+    await createCourse("1", "algebra", 10, 10);
+    const course = await Courses.findOne({ where: { tutorId: "1", courseName: "algebra" } });
+    expect(course).toBeDefined();
+    expect(course).not.toBeNull();
+  });
+});
