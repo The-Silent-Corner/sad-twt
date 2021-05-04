8c3aeb3b44554337da4866182ce3d112cc5485be
Tou Xiong <tvxiong17174@gmail.com>
2021-05-04 17:04:25 -0500

    changed id from random numbers to student/tutor/msg id

--- tests/helpers/Messages/createMessage.test.js
+++ tests/helpers/Messages/createMessage.test.js
@@ -4,28 +4,28 @@ const { createTables, wipeDBTables } = require("../../../db/databaseHelpers");
 
 beforeAll(async() =>{
   await createTables();
-  await createUser("1", "hel", "password", "student");
-  await createUser("2", "jka", "pasd", "tutor");
+  await createUser("studentId", "hel", "password", "student");
+  await createUser("tutorId", "jka", "pasd", "tutor");
 });
 afterAll(async() =>{
   await wipeDBTables();
 });
 describe("creating a message", () =>{
   it("should return true", async() =>{
-    const message = await createMessage("99", "1", "2", "hello");
+    const message = await createMessage("messageId", "studentId", "tutorId", "hello");
     expect(message).toEqual(true);
   });
 });
 describe("message id already exists", () =>{
   it("should return false", async() =>{
-    await createMessage("99", "1", "2", "hello");
-    const message2 = await createMessage("99", "1", "2", "hello");
+    await createMessage("messageId", "studentId", "tutorId", "hello");
+    const message2 = await createMessage("messageId", "studentId", "tutorId", "hello");
     expect(message2).toEqual(false);
   });
 });
 describe("message is not provided", () =>{
   it("should return false", async() =>{
-    const message = await createMessage("99", "1", "2");
+    const message = await createMessage("messageId", "studentId", "tutorId");
     expect(message).toEqual(false);
   });
 });
